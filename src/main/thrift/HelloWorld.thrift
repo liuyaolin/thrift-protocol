fe15d634d995a1f0d9697c8155c0d48ca18f843c
solr <liuyao64@sina.com>
2015-11-09 18:28:11 +0800

    ss

---
+++ src/main/thrift/HelloWorld.thrift
@@ -0,0 +1,15 @@
+namespace  * com.hzins.thrift.demo
+
+service  HelloWorldService{
+  string sayHello(1:required string username)
+}
+
+struct Msg {
+	1:required i64 id; // ID
+	2:optional byte content;//内容
+}
+struct Content {
+	1:required i64 id; // ID
+	2:optional string phone; // 电话
+	3:optional double price; //价格
+}
